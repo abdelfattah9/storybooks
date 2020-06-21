a02d80246b0ccbb214a5188ee127771d7773d91b
Ahmed <ahmedomar1007@gmail.com>
2020-06-21 18:48:45 +0200

    Lanuch App

---
+++ app.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const connectDB = require('./config/db')
+const { Mongoose } = require('mongoose')
+
+// Load Config
+dotenv.config({ path:'./config/config.env' })
+
+// Passport Config
+require('./config/passport')(passport)
+
+// Connect DB
+connectDB()
+
+const app = express();
+
+// Logging
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Handlebars
+app.engine('hbs',exphbs( {layoutsDir:'views/layouts/', defaultLayout:'main', extname:'hbs'} ))
+app.set('view engine', 'hbs')
+
+
+// Session 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore( {mongooseConnection:mongoose.connection} )
+  }))
+
+// Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
